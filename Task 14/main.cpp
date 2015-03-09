826f1602d1a18dc23f9e80ccf4e043e95d74a094
Shemyakov Anton <shemyanton@gmail.com>
2015-03-09 21:23:59 +0300

    MergeSort

---
+++ Task 14/main.cpp
@@ -0,0 +1,49 @@
+#include <cstdlib>
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T>
+T & Merge_Sort(T &arg)
+{
+     /*for(typename T::iterator it = arg.begin(); it != arg.end(); ++it)
+     cout<<(*it)<<"  "; 
+     cout<<endl;      
+     system("PAUSE");*/
+     if(arg.size()!=1)
+     {            
+         list<int> a, b;
+         int n=arg.size(), i=0; 
+         for(typename T::iterator it = arg.begin(); it != arg.end(); ++it)
+         {
+             if(i<n/2)
+                  a.push_back(*it);
+             else
+                  b.push_back(*it);  
+             i++; 
+         }
+         Merge_Sort(a);
+         Merge_Sort(b);
+         merge(a.begin(), a.end(), b.begin(), b.end(), arg.begin());
+         /*for(typename T::iterator it = arg.begin(); it != arg.end(); ++it)
+         cout<<(*it)<<"  ";
+         cout<<endl;*/
+         return arg;
+     }
+     else
+     return arg;
+}
+
+int main(int argc, char *argv[])
+{
+    int a[] = {7, 2, 4, 0, 1, 8, 6, 3, 5, 9};
+    list<int> test(a,a+10);
+    list<int> result = Merge_Sort(test);
+    for(list<int>::iterator it = result.begin(); it != result.end(); ++it)
+    cout<<(*it)<<"  ";
+    cout<<endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
