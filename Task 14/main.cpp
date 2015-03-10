f5c4b9c6040523778c7317bb3776780160c845c4
Shemyakov Anton <shemyanton@gmail.com>
2015-03-10 20:09:08 +0300

    Double stack queue added

--- Task 14/main.cpp
+++ Task 14/main.cpp
@@ -1,9 +1,70 @@
 #include <cstdlib>
 #include <iostream>
+#include <stack>
 #include <list>
 #include <algorithm>
 
 using namespace std;
+
+template <typename T, typename C= std::stack<T> >
+class Queue
+{
+public:
+	Queue(){};
+	bool empty()
+	{
+		return ( In.empty() && Out.empty() );
+	}
+	int size()
+	{
+		return ( In.size() + Out.size() );
+	}
+	T front()
+	{
+		if(!Out.empty())
+			return Out.top();
+		else
+		{
+			Back = In.top();
+			while(!In.empty())
+				{
+					Out.push(In.top());
+					In.pop();
+				}
+		}
+		return Out.top();
+	}
+	T back()
+	{
+		if(!In.empty())
+			return In.top();	
+		else 
+			return Back;
+	}
+	void push(T element)
+	{
+		In.push(element);
+	}
+	void pop()
+	{
+		if(!Out.empty())
+			Out.pop();
+		else
+		{
+		Back = In.top();
+			while(!In.empty())
+				{
+					 Out.push(In.top());
+					 In.pop();
+				}
+			Out.pop();
+		}
+	}
+private:
+	C In;
+	C Out;
+	T Back;
+};
 
 template<typename T>
 T & Merge_Sort(T &arg)
@@ -39,11 +100,25 @@ T & Merge_Sort(T &arg)
 int main(int argc, char *argv[])
 {
     int a[] = {7, 2, 4, 0, 1, 8, 6, 3, 5, 9};
-    list<int> test(a,a+10);
-    list<int> result = Merge_Sort(test);
+    list<int> test1(a,a+10);
+    list<int> result = Merge_Sort(test1);
     for(list<int>::iterator it = result.begin(); it != result.end(); ++it)
     cout<<(*it)<<"  ";
     cout<<endl;
+    
+    Queue<int> test2;
+    test2.push(1);
+    test2.push(2);
+    test2.push(3);
+    test2.push(4);
+    test2.push(5);
+    cout<<"Size of queue is equal to "<<test2.size()<<endl;
+    cout<<test2.front()<<" - "<<test2.back()<<endl;
+    while(!test2.empty())
+    {
+		cout<<test2.front()<<endl;
+		test2.pop();
+	}
     system("PAUSE");
     return EXIT_SUCCESS;
 }
