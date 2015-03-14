71fdc938ebab341f1ea47a0cc70759a4d9c314dc
Shemyakov Anton <shemyanton@gmail.com>
2015-03-14 13:21:32 +0300

    Final solution

--- Task 14/Queue.cpp
+++ Task 14/Queue.cpp
@@ -51,7 +51,7 @@ public:
 			Out.pop();
 		else
 		{
-		Back = In.top();
+			Back = In.top();
 			while(!In.empty())
 				{
 					 Out.push(In.top());
@@ -66,46 +66,8 @@ private:
 	T Back;
 };
 
-template<typename T>
-T & Merge_Sort(T &arg)
-{
-     /*for(typename T::iterator it = arg.begin(); it != arg.end(); ++it)
-     cout<<(*it)<<"  "; 
-     cout<<endl;      
-     system("PAUSE");*/
-     if(arg.size()!=1)
-     {            
-         list<int> a, b;
-         int n=arg.size(), i=0; 
-         for(typename T::iterator it = arg.begin(); it != arg.end(); ++it)
-         {
-             if(i<n/2)
-                  a.push_back(*it);
-             else
-                  b.push_back(*it);  
-             i++; 
-         }
-         Merge_Sort(a);
-         Merge_Sort(b);
-         merge(a.begin(), a.end(), b.begin(), b.end(), arg.begin());
-         /*for(typename T::iterator it = arg.begin(); it != arg.end(); ++it)
-         cout<<(*it)<<"  ";
-         cout<<endl;*/
-         return arg;
-     }
-     else
-     return arg;
-}
-
 int main(int argc, char *argv[])
 {
-    int a[] = {7, 2, 4, 0, 1, 8, 6, 3, 5, 9};
-    list<int> test1(a,a+10);
-    list<int> result = Merge_Sort(test1);
-    for(list<int>::iterator it = result.begin(); it != result.end(); ++it)
-    cout<<(*it)<<"  ";
-    cout<<endl;
-    
     Queue<int> test2;
     test2.push(1);
     test2.push(2);
