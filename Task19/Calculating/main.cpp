b48be228236b67427fd69b29854e208bbad76947
Shemyakov Anton <shemyanton@gmail.com>
2015-05-16 17:14:04 +0300

    Assembler calculation
    Done in linear time

--- Task19/Calculating/main.cpp
+++ Task19/Calculating/main.cpp
@@ -2,63 +2,42 @@
 
 int main()
 {
-	int n /*= 2*/, result1, one;
-	double x /*= 1*/, a /*= 3*/, result, result2, trash;
-	std::cout << "Enter n, x and a\n";
-	std::cin >> n >> x >> a;
+	int n = 2, temp;
+	double x = 1, a = 3, result;
+	//std::cout << "Enter n, x and a\n";
+	//std::cin >> n >> x >> a;
 	_asm
 	{
-		mov one, 1
-		mov ecx, n
-		shl ecx, 1
-		finit
+		fld1// result
+		fld1// temp numerator
+		fld1// temp denominator
 
-		fild one// result
+		mov ecx, n
+		shl ecx, 1//ecx = 3n
 
 		xor ebx, ebx
-		xor eax, eax
-		inc eax
-		//Set result2 to 1
-		fild one//result2
+
 cycle:
-		inc ebx//Counter
-		mov esi, ebx
-		//add esi, n
-		mul esi
-		mov edx, n
-		cmp ebx, edx
+		
+		inc ebx//i = ebx
+		mov temp, ebx
+		fild temp
+		fmulp ST(1), ST(0)
+		cmp ebx, n
 		jng cycle
 
 		fld x
 		fadd a
-		fmul ST(1), ST(0)
-		//pop ST(0)
-		fstp trash
+		fmulp ST(2), ST(0)
 
-		mov esi, n
-		add esi, n
-		add esi, n
-		sub esi, ecx
-		inc esi
-		cmp ebx, esi
-		jne cycle
-
-		mov result1, eax
-		fild result1
-		fdivr ST, ST(1)
-		fmul ST, ST(2)
 		fxch ST(2)
-		//pop stack
-		fstp trash
+		fmul ST(0), ST(1)
+		fdiv ST(0), ST(2)
+		fxch ST(2)
 
-		xor ebx, ebx
-		xor eax, eax
-		inc eax
-		//Set result2 to 1
-		fstp trash
-		fild one
 		loop cycle
-		fstp trash
+
+		fxch ST(2)
 		fst result
 	}
 	std::cout << result <<"\n";
